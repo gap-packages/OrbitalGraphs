81ad4b6b473215626f564c2a153be9d305add173
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2019-09-11 15:33:33 +0100

    Catching up

--- gap/OrbitalGraphs.gd
+++ gap/OrbitalGraphs.gd
@@ -6,10 +6,28 @@
 
 #! @Description
 #!   This attribute is an immutable list of all orbital graphs of
-#!   a transformation semigroup or permutation group
+#!   a transformation semigroup or permutation group.
 DeclareAttribute("OrbitalGraphs", IsPermGroup);
+
+#! @Description
+#!  Computes the orbital closure of a permutation group by constructing
+#!  a single edge-coloured graph from all orbital graphs by assigning an edge
+#!  colour to each orbital graph and taking the uninion of these coloured edges
+#!  to obtain the resulting graph.
+#!  The automorphism group of this graph is the same as the intersecion of the
+#!  automorphism groups of all orbital graphs, but in some cases where a group
+#!  has many orbital graphs, the direct computation of the automorphism group
+#!  of the edge-coloured graph might be more efficient than first computing
+#!  separate automorphism groups and then intersecting them in a separate step.
+#!
+#!  Since this is an experimental feature we just provide a separate function
+#!  and don't hook this method up yet, before we evaluated whether this method
+#!  works properly and mostly faster.
+DeclareGlobalFunction("OrbitalClosureByColouredEdges");
+
 #DeclareAttribute("OrbitalGraphsRepresentative", IsPermGroup);
 DeclareAttribute("OrbitalGraphs", IsTransformationSemigroup);
+
 
 #! @Description
 #!   The <E>orbital closure</E> of a permutation group <M>G</M>
